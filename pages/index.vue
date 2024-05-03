ee2415b5b2446f3813d6749c671c787c3331dbb9
Albert Pratomo <albertpratomo21@gmail.com>
2024-05-04 00:34:12 +0200

    feat: enable swapping post

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 const { data } = await useFetch<{ id: number }>('https://jsonplaceholder.typicode.com/posts')
 
-const posts = ref(data.value.slice(0, 5).map(p => `Post ${p.id}`))
+const posts = ref(data.value.slice(0, 5).map(p => p.id))
+
+function swap(from: number, to: number) {
+    const temp = posts.value[from]
+    posts.value[from] = posts.value[to]
+    posts.value[to] = temp
+}
 </script>
 
 <template>
@@ -12,11 +18,25 @@ const posts = ref(data.value.slice(0, 5).map(p => `Post ${p.id}`))
 
                 <ul class="mt-4 flex flex-col gap-4">
                     <li
-                        v-for="post in posts"
+                        v-for="(post, i) in posts"
                         :key="post"
-                        class="rounded bg-white px-2 py-4 text-black shadow"
+                        class="h-20 flex items-center justify-between rounded bg-white p-2 text-black shadow"
                     >
-                        {{ post }}
+                        Post {{ post }}
+
+                        <div class="flex flex-col gap-3">
+                            <button
+                                v-if="i > 0"
+                                class="i-carbon-chevron-up"
+                                @click="swap(i, i - 1)"
+                            />
+
+                            <button
+                                v-if="i < posts.length - 1"
+                                class="i-carbon-chevron-down"
+                                @click="swap(i, i + 1)"
+                            />
+                        </div>
                     </li>
                 </ul>
             </div>
