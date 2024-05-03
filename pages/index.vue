7510038589d1a7325d34c635b623bbc76ad0ad93
Albert Pratomo <albertpratomo21@gmail.com>
2024-05-03 23:48:13 +0200

    feat: render 5 posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,29 @@
 <script setup lang="ts">
-const online = useOnline()
+const { data } = await useFetch<{ id: number }>('https://jsonplaceholder.typicode.com/posts')
+
+const posts = ref(data.value.slice(0, 5).map(p => `Post ${p.id}`))
 </script>
 
 <template>
-  <div>
-    <Logos mb-6 />
-    <Suspense>
-      <ClientOnly>
-        <PageView v-if="online" />
-        <div v-else text-gray:80>
-          You're offline
+    <div class="max-w-2xl p-8">
+        <div class="grid grid-cols-2 gap-16">
+            <div>
+                <h1>Sortable Post List</h1>
+
+                <ul class="mt-4 flex flex-col gap-4">
+                    <li
+                        v-for="post in posts"
+                        :key="post"
+                        class="rounded bg-white px-2 py-4 text-black shadow"
+                    >
+                        {{ post }}
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                List of actions committed
+            </div>
         </div>
-      </ClientOnly>
-      <template #fallback>
-        <div italic op50>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
-  </div>
+    </div>
 </template>
