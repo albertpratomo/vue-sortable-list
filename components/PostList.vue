1c3a46db741af7b3ff4709c1f39ff96b925d1602
Albert Pratomo <albertpratomo21@gmail.com>
2024-05-09 23:29:23 +0200

    refactor: separate PostItem component

--- components/PostList.vue
+++ components/PostList.vue
@@ -22,29 +22,14 @@ await useAsyncData('posts', () => fetchPosts())
                 class="mt-4 flex flex-col gap-4"
                 aria-label="Sortable Post List"
             >
-                <li
+                <PostItem
                     v-for="(post, i) in posts"
+                    :id="post"
                     :key="post"
-                    class="h-20 flex items-center justify-between rounded bg-zinc-100 px-3 py-2 text-zinc-600 shadow-lg"
-                >
-                    Post {{ post }}
-
-                    <div class="flex flex-col gap-3 text-indigo-600">
-                        <button
-                            v-if="i > 0"
-                            class="i-heroicons-chevron-up-16-solid hover:text-indigo-950"
-                            :aria-label="`Post ${post} swap up button`"
-                            @click="swap(i, i - 1)"
-                        />
-
-                        <button
-                            v-if="i < posts.length - 1"
-                            class="i-heroicons-chevron-down-16-solid hover:text-indigo-950"
-                            :aria-label="`Post ${post} swap down button`"
-                            @click="swap(i, i + 1)"
-                        />
-                    </div>
-                </li>
+                    :can-swap-up="i > 0"
+                    :can-swap-down="i < posts.length - 1"
+                    @swap="swap(i, i + $event)"
+                />
             </ul>
         </div>
     </Suspense>
